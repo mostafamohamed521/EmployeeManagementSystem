0cada06525c4316eb598ec47abff7411bb0abda9
Desha <mostafamohamed92895@gmail.com>
2026-09-07 10:35:54 +0300

    Add Employee entity model

---
+++ EmployeeManagementSystem/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeManagementSystem.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Salary { get; set; }
+    public int DepartmentId { get; set; }
+    public Department Department { get; set; } = null!;
+    public ICollection<Project> Projects { get; set; } = new List<Project>();
+}
