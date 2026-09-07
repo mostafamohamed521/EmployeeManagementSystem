da0e2661ccc0ad171520b4238f54236548fb73ae
Desha <mostafamohamed92895@gmail.com>
2026-09-07 10:35:46 +0300

    Add Department entity model

---
+++ EmployeeManagementSystem/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeManagementSystem.Models;
+
+public class Department
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+}
